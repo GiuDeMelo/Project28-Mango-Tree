aaca22ed74ffeae21c55b0cc5388ce2d20d508e5
Giu_ <88463562+GiuDeMelo@users.noreply.github.com>
2022-12-06 09:33:28 -0300

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -1,25 +1,22 @@
-class Stone
-{
-	constructor(x,y,r)
-	{
+class Stone{
+	constructor(x,y,r){
 		var options={
 			isStatic:false,
 			restitution:0,
 			friction:1,
 			density:1.2
-			}
+		}
 		this.x=x;
 		this.y=y;
 		this.r=r
 
-		this.image=loadImage("images/stone.png");
+		this.image=loadImage("stone.png");
 
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
 		World.add(world, this.body);
 	}
 
-	display()
-	{
+	display(){
 			var stonepos=this.body.position;		
 			push()
 			translate(stonepos.x, stonepos.y);
@@ -30,7 +27,6 @@ class Stone
 			ellipseMode(RADIUS)
 			image(this.image, 0,0,this.r*2, this.r*2)
 			pop()
-			
 	}
 
 }
